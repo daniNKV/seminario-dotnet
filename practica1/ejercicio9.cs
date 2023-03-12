fd885b644682ab414e68e4b9b78c1995cb9512e0
daniNKV <nikiforovdaniel@gmail.com>
2023-03-12 03:02:51 -0300

    Practica 1: Ejercicio 9

---
+++ practica1/ejercicio9.cs
@@ -0,0 +1,23 @@
+/*
+Escribir un programa que lea dos palabras separadas por un blanco que terminan con <ENTER>,
+y determinar si son simétricas (Ej: 'abbccd' y 'dccbba' son simétricas).
+Tip: si st es un string, entonces st[0] devuelve el primer carácter de st, y st[st.Length-1]
+devuelve el último carácter de st
+*/
+
+Console.WriteLine("Ingrese dos cadenas de texto separadas por espacio: ");
+string cadena = Console.ReadLine();
+
+bool simetricas = false;
+
+for (int i = 0; i < cadena.Length / 2; i++) {
+    if (cadena[i] == cadena[cadena.Length - 1 - i]) {
+        simetricas = true;
+    }
+    else {
+        simetricas = false;
+        break;
+    }
+}
+
+Console.WriteLine(simetricas);
