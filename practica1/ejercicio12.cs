58113e02087b13367542edf38c2448b4325a9a34
daniNKV <nikiforovdaniel@gmail.com>
2023-03-12 03:25:08 -0300

    Practica 1: Ejercicio 12

---
+++ practica1/ejercicio12.cs
@@ -0,0 +1,20 @@
+/*
+Escribir un programa que imprima todos los divisores de un número entero ingresado desde la
+consola. Para obtener el entero desde un string st utilizar int.Parse(st)
+*/
+int num;
+Console.WriteLine("Ingrese un número entero: ");
+num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < num; i++) {
+    if (num % i == 0) {
+        Console.WriteLine(i);
+    }
+}
+
+
+
+
+
+
+
