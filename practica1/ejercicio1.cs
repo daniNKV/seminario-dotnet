7e1cf8d393ba7652ad34f6f54714f30667e3868d
daniNKV <nikiforovdaniel@gmail.com>
2023-03-12 00:14:18 -0300

    Practica 1: Ejercicio 1

---
+++ practica1/ejercicio1.cs
@@ -0,0 +1,20 @@
+// Investigar por las secuencias de escape \n, \t , \" y \\. Escribir un programa que las utilice
+// para imprimir distintos mensajes en la consola.
+
+
+// \n Salto de línea
+// \t Carácter de Tabulación
+// \ Escape de carácteres no reconocidos o inicio de expresion regular con \{
+// \\ Escapa al carácter \
+
+using System;
+
+class Ejercicio2
+{
+    public static void Main()
+    {
+        Console.WriteLine("Esta linea se corta con \n un escape");
+        Console.WriteLine("Esta linea tiene un caracter \t de tabulación");
+        Console.WriteLine("Esta linea acepta caracteres \\n no reconocidos");
+    }
+}
