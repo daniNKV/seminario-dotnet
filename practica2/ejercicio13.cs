6859c434c8644943ced6f99c069b0884a3b8cb77
daniNKV <nikiforovdaniel@gmail.com>
2023-03-22 16:01:11 -0300

    Corrige ejercicio 13

---
+++ practica2/ejercicio13.cs
@@ -0,0 +1,47 @@
+// Definir el tipo de datos enumerativo llamado Meses y utilizarlo para:
+// a) Imprimir en la consola el nombre de cada uno de los meses en orden inverso (diciembre,
+// noviembre, octubre ..., enero)
+// c) Solicitar al usuario que ingrese un texto y responder si el texto tipeado corresponde al
+// nombre de un mes
+// Nota: en todos los casos utilizar un for iterando sobre una variable de tipo Meses
+
+class ejercicio12 
+{
+    enum Meses 
+    {
+        Enero,
+        Febrero,
+        Marzo,
+        Abril,
+        Mayo,
+        Junio,
+        Julio,
+        Agosto,
+        Septiembre,
+        Octubre,
+        Noviembre,
+        Diciembre
+    }
+    static void ListarMeses()
+    {
+        Meses meses = Enum.GetNames(typeof(Meses));
+
+        for (int i=meses.Length; i > 0; i--) { 
+            Console.println(mes);
+        }
+    }
+
+    static void CheckMes()
+    {
+        Console.println("Ingrese un mes");
+        string mesBuscado = Console.readln();
+
+        foreach(string mes in meses){
+            if(mesBuscado == mes ) {
+                Console.println("Encontrado");
+            }else {
+                Console.println("No encontrado");
+            }
+        }
+    }
+}
