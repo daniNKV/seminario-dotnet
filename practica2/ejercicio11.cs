dcea439d395da9923fb4647b37150aaffd47a949
daniNKV <nikiforovdaniel@gmail.com>
2023-03-22 12:51:29 -0300

    Practica 2: ejercicio 11

---
+++ practica2/ejercicio11.cs
@@ -0,0 +1,18 @@
+// ¿Para qué sirve el método Split de la clase string? Usarlo para escribir en la consola todas
+// las palabras (una por línea) de una frase ingresada por consola por el usuario
+
+// Split es un método de la clase String, que retorna un array con las substrings
+// de la instancia string desde la cual fue llamado, usando un delimitador
+// ingresado como parámetro
+
+class ejercicio8
+{
+    static void partirString(string frase)
+    {
+        string[] palabras = frase.Split(' ');
+
+        palabras.forEach(string palabra in palabras) {
+            Console.println(palabra);
+        }
+    }
+}
