7ca7ee77130cc6d6245a61ef2b33877060075686
daniNKV <nikiforovdaniel@gmail.com>
2023-03-26 00:54:19 -0300

    Swap de valores por referencia, ej 18

---
+++ practica2/ejercicio18.cs
@@ -0,0 +1,11 @@
+// Codificar el método Swap que recibe 2 parámetros enteros e intercambia sus valores. El cambio
+// debe apreciarse en el método invocador.
+
+class Ejercicio18 
+{
+    static void Swap(object a, object b){
+        object temp = a;
+        a = b;
+        b = temp;
+    }
+}
