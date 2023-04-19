81ba32dcd4ba7a62835e39bd7aca67cbfc6e62bf
daniNKV <nikiforovdaniel@gmail.com>
2023-04-19 11:45:41 -0300

    Corrige detalles y agrega miembros estáticos faltantes

--- practica5/ejercicio1/Cuenta.cs
+++ practica5/ejercicio1/Cuenta.cs
@@ -1,5 +1,10 @@
 class Cuenta 
 {
+    private static double depositos;
+    private static double totalDepositos;
+    private static double extracciones;
+    private static double totalExtracciones;
+    private static int denegados;
     private static int cuentas;
     private int _id;
     private double _saldo;
@@ -7,6 +12,11 @@ class Cuenta
     static Cuenta()
     {
         cuentas = 0;
+        depositos = 0;
+        extracciones = 0;
+        totalDepositos = 0;
+        totalExtracciones = 0;
+        denegados = 0;
     }
 
     public Cuenta() 
@@ -14,7 +24,7 @@ class Cuenta
         this._saldo = 0;
         this.cuentas += 1;
         this._id = this.cuentas;
-        Console.WriteLine($"Se creó la cuenta ID = {id}");
+        Console.WriteLine($"Se creó la cuenta ID = {this._id}");
     } 
 
     public void Extraer(double monto) 
@@ -24,15 +34,21 @@ class Cuenta
         } catch (e) 
         {
             Console.WriteLine(e.message);
+            Cuenta.denegados++;
         }
-        Console.WriteLine($"Se extrajo {monto} en la cuenta {this._id}");
+
+        Cuenta.extracciones++;
+        Cuenta.totalExtracciones += monto; 
+        Console.WriteLine($"Se extrajo {monto} en la cuenta {this._id} (Saldo = {this._saldo})");
         return this;
     }
 
     public void Depositar(double monto)
     {
         this._saldo += monto;
-        Console.WriteLine($"Se depositaron {monto} en la cuenta {this._id}");
+        Cuenta.depositos++;
+        Cuenta.depositosTotal += monto;
+        Console.WriteLine($"Se depositaron {monto} en la cuenta {this._id} ${(Saldo= this._saldo)}");
         
         return this;
     }
@@ -45,5 +61,16 @@ class Cuenta
         }
 
         this._saldo -= monto;
+    }
+
+    static void ImprimirDetalle()
+    {
+        Console.WriteLine("DETALLE");
+        Console.WriteLine($"CUENTAS CREADAS: {this.cuentas}");
+        Console.WriteLine($"Depósitos: {this.cuentas} - Total depositado: {this.totalDepositos}");
+        Console.WriteLine($"Extracciones: {this.extracciones} - Total extraido: {this.totalExtracciones}");
+        Console.WriteLine($"Saldo {this.depositos - this.extracciones}");
+        Console.WriteLine($"Se denegaron {this.denegados} extracciones por falta de fondos");
+        
     }
 }
