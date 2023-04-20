12e67f29cbc80c24734c7b0272dce87448f61cfa
daniNKV <nikiforovdaniel@gmail.com>
2023-04-20 20:43:12 -0300

    Mejora estructura polimórfica ej7

---
+++ practica6/ejercicio7/IImprimible.cs
@@ -0,0 +1,4 @@
+interface IImprimible
+{
+    void Imprimir();
+}
--- practica6/ejercicio7/ejercicio7.cs
+++ practica6/ejercicio7/ejercicio7.cs
@@ -1,25 +1,24 @@
 // 7) Ofrecer una implementación polimórfica para mejorar el siguiente programa:
 
 Imprimidor.Imprimir(new A(), new B(), new C(), new D());
-class A {
-public void ImprimirA() => Console.WriteLine("Soy una instancia A");
+
+
+class A: IImprimible {
+    public void Imprimir() => Console.WriteLine("Soy una instancia A");
 }
-class B {
-public void ImprimirB() => Console.WriteLine("Soy una instancia B");
+class B: IImprimible {
+    public void Imprimir() => Console.WriteLine("Soy una instancia B");
 }
-class C {
-public void ImprimirC() => Console.WriteLine("Soy una instancia C");
+class C: IImprimible {
+    public void Imprimir() => Console.WriteLine("Soy una instancia C");
 }
-class D {
-public void ImprimirD() => Console.WriteLine("Soy una instancia D");
+class D: IImprimible {
+    public void Imprimir() => Console.WriteLine("Soy una instancia D");
 }
 static class Imprimidor {
-public static void Imprimir(params object[] vector) {
-foreach (object o in vector) {
-if (o is A) { (o as A)?.ImprimirA(); }
-else if (o is B) { (o as B)?.ImprimirB(); }
-else if (o is C) { (o as C)?.ImprimirC(); }
-else if (o is D) { (o as D)?.ImprimirD(); }
-}
-}
+    public static void Imprimir(params IImprimible[] vector) {
+        foreach (IImprimible o in vector) {
+            o.Imprimir();
+        }
+    }
 }
